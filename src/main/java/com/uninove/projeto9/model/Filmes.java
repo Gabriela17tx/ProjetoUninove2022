707c25358e433d5bd462a3997e9e734a010d7668
gabriela <gabriela17teixeira@gmail.com>
2022-06-06 09:09:49 -0300

    [Feature Gabriela - Task: configure appplication.properties]

---
+++ src/main/java/com/uninove/projeto9/model/Filmes.java
@@ -0,0 +1,35 @@
+package com.uninove.projeto9.model;
+
+public class Filmes {
+
+	private long id;
+	
+	private String nomeDoFilme;
+	
+	private String categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeDoFilme() {
+		return nomeDoFilme;
+	}
+
+	public void setNomeDoFilme(String nomeDoFilme) {
+		this.nomeDoFilme = nomeDoFilme;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+}
---
+++ src/main/java/com/uninove/projeto9/model/Usuario.java
@@ -0,0 +1,9 @@
+package com.uninove.projeto9.model;
+
+public class Usuario {
+	private Long idUsuario;
+	private String nome;
+	private String email;
+	private String senha; 
+
+}
